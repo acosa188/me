8532457dbefe2c635993439948856b27872f9996
Arjun Cosare <arjun.cosare@outlook.com>
2020-10-21 16:59:11 -0600

    About me section is responsive till 1200px wide

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,6 +6,23 @@ const AboutMeContainer = styled.div`
     margin-top: 400px;
     width: 39.375rem;
     min-height: 31.9375rem;
+
+    @media (min-width: 1200px){
+        margin-top: 200px;
+    }
+
+    @media (min-width: 1400px) {
+        margin-top: 300px;
+    }
+
+    @media (min-width: 1700px) {
+        margin-top: 450px;
+    }
+
+    @media (min-width: 1910px) {
+        width: 39.375rem;
+        min-height: 31.9375rem;
+    }
 `;
 
 const HeadingLabel = styled.p`
@@ -13,10 +30,50 @@ const HeadingLabel = styled.p`
     font-family: 'Kumbh Sans', sans-serif;
     font-size: 2.5rem;
     font-weight: bold;
+
+    @media (min-width: 1200px){
+        font-size: 1.7rem;
+        width: 340px;
+    }
+
+    @media (min-width: 1400px) {
+        font-size: 2.1rem;
+        width: 400px;
+    }
+
+    @media (min-width: 1700px) {
+        font-size: 2rem;
+        width: 440px;
+    }
+
+    @media (min-width: 1910px){
+        width: 500px;
+        font-size: 2.5rem;
+    }
 `;
 
 const Information = styled.p`
-    font-size: 26px;
+    font-size: 1.625rem;
+    
+    @media (min-width: 1200px){
+        font-size: 1rem;
+        width: 350px;
+    }
+
+    @media (min-width: 1400px) {
+        font-size: 1.1rem;
+        width: 450px;
+    }
+
+    @media (min-width: 1700px) {
+        font-size: 1.2rem;
+        width: 450px;
+    }
+
+    @media (min-width: 1910px){
+        font-size: 1.625rem;
+        width: 550px;
+    }
 `;
 
 const HorizontalLine = styled.hr`
@@ -29,6 +86,35 @@ const HorizontalLine = styled.hr`
     width: 200px;
     transform: rotate(270deg);
     border-top: 3px solid #707070;
+
+    @media (min-width: 1200px){
+        left: 53px;
+        top: 180px;
+        width: 100px;
+    }
+
+    @media (min-width: 1400px) { 
+        top: 210px;
+        width: 120px;
+    }
+
+    @media (min-width: 1700px) {
+        left: 40px;
+        top: 230px;
+        width: 150px;
+    }
+
+    @media (min-width: 1910px){
+        position: absolute;
+        top: 280px;
+        left: 0;
+
+        margin-left: -85px;
+
+        width: 200px;
+        transform: rotate(270deg);
+        border-top: 3px solid #707070;
+    }
 `;
 
 const Aside = styled.div`
@@ -51,6 +137,35 @@ const AboutMeLabel = styled.p`
     transform: rotate(270deg);
     transform-origin: (0 0);
     margin-left: -60px;
+
+    @media (min-width: 1200px){
+        left: 20px;
+        top: 55px;
+        font-size: 1.7rem;
+    }
+
+    @media (min-width: 1400px) {
+        font-size: 1.9rem;
+        top: 60px;
+    }
+
+    @media (min-width: 1700px) {
+        left: 20px;
+        top: 65px;
+        font-size: 2rem;
+    }
+
+    @media (min-width: 1910px){
+        position: absolute;
+        top: 75px;
+        left: 0;
+        font-size: 2.1875rem;
+        font-weight: 500;
+        color: #707070;
+        transform: rotate(270deg);
+        transform-origin: (0 0);
+        margin-left: -60px;
+    }
 `;
 
 const AboutMe = () => {
